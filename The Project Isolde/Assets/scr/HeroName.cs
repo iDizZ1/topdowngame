8765a873ef991c9b9e34b8aee22c519221536573
CharaWein <Weinmaier-Andrew@yandex.ru>
2024-03-06 21:59:34 +0300

    chto-to
    chtot-to ochen uzasnoe

---
+++ The Project Isolde/Assets/scr/HeroName.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HeroName : MonoBehaviour
+{
+    public TMP_Text Textfield;
+    public string Hname;
+    
+    void Update()
+    {
+       Textfield.text = Hname;
+    }
+}
