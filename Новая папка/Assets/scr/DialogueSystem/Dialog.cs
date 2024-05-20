4081c05aec9fca5df82dbc10c83a657f789ae231
iDizZ1 <idizz100@gmail.com>
2024-05-20 10:38:31 +0300

    начальная локация, доработанные диалоги

--- Новая папка/Assets/scr/DialogueSystem/Dialog.cs
+++ Новая папка/Assets/scr/DialogueSystem/Dialog.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Dialog : MonoBehaviour
 {
-    public GameObject DialogObject;
     public GameObject DialogObject1;
+    public GameObject DialogObject2;
 
         void Update()
         
         {
             if (Input.GetKeyDown(KeyCode.B))
             {
-                DialogObject.SetActive(true);
-                DialogObject1.SetActive(false);
+                DialogObject1.SetActive(true);
+                DialogObject2.SetActive(false);
             }
             if (Input.GetKeyDown(KeyCode.N))
             {
-                DialogObject.SetActive(false);
-                DialogObject1.SetActive(true);
+                DialogObject1.SetActive(false);
+                DialogObject2.SetActive(true);
             }
    
 
